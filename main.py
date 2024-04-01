9b5711dedd4abbd8d2029d57327d0e7c1d06f54a
Omurbek Dulatov <oma.dulatov@gmail.com>
2024-04-01 19:49:33 +0600

    task-2 done

--- main.py
+++ main.py
@@ -7,11 +7,23 @@
 """
 
 
-cities = ['MOSKOW', 'BISHKEK', 'ALMATY', 'BERLIN', 'ANKARA', 'AMSTERDAM']
+# cities = ['MOSKOW', 'BISHKEK', 'ALMATY', 'BERLIN', 'ANKARA', 'AMSTERDAM']
 
 # def make_to_lower(city_name):
 #     return city_name.lower()
 
-cities_lower = list(map(lambda city_name: city_name.lower(), cities))
+# cities_lower = list(map(lambda city_name: city_name.lower(), cities))
 
-print(cities_lower)
+# print(cities_lower)
+
+
+"""
+2) Отфильтруйте отобрав в новый список только четные числа 
+[4, 3, 6, 8, 9, 7, 1, 2, 34, 5, 56, 76], 
+и возведите в третью степень каждую цифру отфильтрованного списка.
+"""
+
+numbers = [4, 3, 6, 8, 9, 7, 1, 2, 34, 5, 56, 76]
+numbers = filter(lambda x: x % 2 == 0, numbers)
+numbers = list(map(lambda x: pow(x, 3), numbers))
+print(numbers)
